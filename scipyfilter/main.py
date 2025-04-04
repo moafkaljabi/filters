e66b41836695d966d08a90538767e8cd2d4e3279
Moafk Aljabi <mwafak_sh@hotmail.com>
2025-04-05 00:51:25 +0200

    ploting matrices

--- scipyfilter/main.py
+++ scipyfilter/main.py
@@ -1,2 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
-print ("main")
+
+
+# Use an interactive backend
+plt.switch_backend('TkAgg')  
+
+n = 10000
+x = np.linspace(0,10,n+1)
+y = np.exp(-x /10) * np.sin(x)
+
+plt.plot(x,y)
+plt.show()
